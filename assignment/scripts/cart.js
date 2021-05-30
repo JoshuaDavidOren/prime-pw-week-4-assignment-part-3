7530232f49f15ff0b502df3f351109a720b8f5d5
Joshua Oren <joshuadavidoren@gmail.com>
2021-05-30 09:31:58 -0500

    added empty atempt

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,7 +1,7 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
-let basket = [];
+let basket = ['candy','morecandy','lesscandy'];
 function addItem( item ) {
   basket.push( item );
   return true;
@@ -11,8 +11,20 @@ addItem( 'food')
 addItem( 'food2')
 addItem( 'food3')
 console.log(basket)
+console.log(`Basket is ${basket}`);
 
-function listItems(basket) {
-  for( let i = 0; i < basket.length; i++)
-  console.log(basket[i]);
+function listItems() {
+  for( i = 0; i < basket.length; i++)
+    return console.log(basket[i]);
+  }
+
+function empty() {
+  for( i in basket, basket.pop)
+   i = basket.length-1;
+   i >=0
+  return noItems
 }
+
+  console.log(`Basket is ${basket}`);
+  console.log('Adding apples (expect true)', addItem('apples'));
+  console.log(`Basket is now ${basket}`);
