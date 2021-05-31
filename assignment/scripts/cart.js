7d37db0b05ac58fcc8f37860a48d364f99563a71
Joshua Oren <joshuadavidoren@gmail.com>
2021-05-31 15:17:58 -0500

    LATE!! but i finnally got it to work.

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,29 +2,21 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 let basket = ['candy','morecandy','lesscandy'];
+
 function addItem( item ) {
   basket.push( item );
   return true;
-
 }
-addItem( 'food')
-addItem( 'food2')
-addItem( 'food3')
-console.log(basket)
-console.log(`Basket is ${basket}`);
+
 
 function listItems() {
-  for( i = 0; i < basket.length; i++)
-    return console.log(basket[i]);
-  }
-
-function empty() {
-  for( i in basket, basket.pop)
-   i = basket.length-1;
-   i >=0
-  return noItems
+  for( let i = 0; i < basket.length-1; i++) {
+    console.log(basket[i]);
+  }return console.log('Item list');
 }
 
-  console.log(`Basket is ${basket}`);
-  console.log('Adding apples (expect true)', addItem('apples'));
-  console.log(`Basket is now ${basket}`);
+  function empty() {
+    while(basket.length > 0) {
+      basket.pop();
+    } return console.log('No items in cart');
+  }
